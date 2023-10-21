de633cbc898df096337fe701b098bd870d5d3084
EdgarGs21 <eger.2110gs@gmail.com>
2023-10-20 21:53:50 -0600

    aplicacionGui
    Ya se puede convertir la imagen a escala de grises indexada y asi.
    ahora estoy con la ecualizacion y los histogramas

--- Guardar.m
+++ Guardar.m
@@ -1,13 +1,14 @@
 %Guarda la imagen procesada por el usuario
+
+  function Guardar(img, map, type)
+
 %    img: matriz de la imagen a guardar
 %    map: mapa de color de la imagen
 %    type: tipo de la imagen grayscale, binary, RGB, indexed)
 
-    function Guardar(img, map, type)
-
         % Terminar si no hay imagen cargada
         if isempty(img) 
-            return 
+            return; 
         end
 
     % Obtener ruta del archivo donde guardar
@@ -16,17 +17,21 @@
 
         % Terminar si se cierra la ventana
         if file == 0
-            return 
+            return;
         end
 
     % Guardar imagen indexada con mapa de color si es posible
     path = fullfile(folder, file);
-        if type == "indexed" && endsWith(path, [".bmp",".tif"])
-            imwrite(img, map, path);
+
+        % Verificar el tipo de imagen y el formato de archivo
+        if type == "indexed" && any(endsWith(path, [".bmp",".tif"]))
+            
+            % Guardar imagen indexada con mapa de color
+            imwrite(img, map, path); 
 
             % Guardar imagen sin mapa de color
         else 
             imwrite(img, path);
         end
-
+        
         msgbox('Imagen guardada con exito!');
