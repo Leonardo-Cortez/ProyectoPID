341116744771faff882dec307ef17f332e9ffac4
Leonardo Cortez <leonardocortez389@gmail.com>
2023-11-10 14:46:24 -0600

    Botón deshacer todo y función de inversión binaria
    Se ha agregado un botón extra para deshacer todo, es decir, la única diferencia con el deshacer "normal", es que va regresar a la imagen original (en la sección de imagen procesada) después de no querer cualquier filtro.
    También se ha agregado la función para hacer la inversión binaria.

---
+++ InversionBinaria.m
@@ -0,0 +1,25 @@
+
+% Invierte imagen binaria aplicando inversion binaria.
+% 
+% Entradas:
+%       img:  imagen a invertir
+%       type: tipo de la imagen a invertir
+% Output:
+%       imagenBinaria: imagen invertida
+
+function imagenBinaria = InversionBinaria(img, type)
+
+% Excepciones
+if ~(type == "binary")
+    mensajeError = MException('Image:TypeError', 'ERROR: Antes de hacer la inversión binaria, verifique que su imagen sea de tipo Binaria');
+    throw(mensajeError)
+end
+
+%{ 
+    Funcionalidad
+    Realiza la inversión binaria de la imagen. 
+    Resta cada valor de píxel en la imagen original (img) de 1. 
+    Esto invierte los píxeles negros (con valor 0) a blancos (con valor 1) y viceversa.
+%}
+
+imagenBinaria = 1 - img;
